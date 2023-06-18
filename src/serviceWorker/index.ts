b2b0f11c8085174ef2b3badd931a9fa8327aaa69
rajput-karsh <utkarsh1@appinventiv.com>
2023-06-18 23:30:42 +0530

    fix: service worker intialization issue

--- src/serviceWorker/index.ts
+++ src/serviceWorker/index.ts
@@ -10,7 +10,7 @@ const isLocalhost = Boolean(
   );
   
   export function register(config: {[key: string]: any}) {
-    if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator) {
+    if ("serviceWorker" in navigator) {
       // The URL constructor is available in all browsers that support SW.
       const publicUrl = new URL(window.location.href);
       if (publicUrl.origin !== window.location.origin) {
@@ -21,7 +21,7 @@ const isLocalhost = Boolean(
       }
   
       window.addEventListener("load", () => {
-        const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+        const swUrl = `/service-worker.js`;
   
         if (isLocalhost) {
           // This is running on localhost. Let's check if a service worker still exists or not.
