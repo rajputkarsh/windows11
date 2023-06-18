6023a4d758306d95231bf5016067aefb51bac0c0
rajput-karsh <utkarsh1@appinventiv.com>
2023-06-18 23:43:32 +0530

    fix: update imports in service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -15,7 +15,7 @@ workbox.core.clientsClaim();
 // Their URLs are injected into the manifest variable below.
 // This variable must be present somewhere in your service worker file,
 // even if you decide not to use precaching. See https://cra.link/PWA
-workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+// workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
 // Set up App Shell-style routing, so that all navigation requests
 // are fulfilled with your index.html shell. Learn more at
